d01c61dffc08bbc0b1d310705c84f0e6cc295b5b
RIASLAB <117793990+RIASLAB@users.noreply.github.com>
2024-10-16 21:31:09 -0500

    WHILE.java

--- WHILE.java
+++ WHILE.java
@@ -1,23 +1,22 @@
 import java.util.Scanner;
 
-public class WHILE {
+public class naturales {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
+        Scanner scanner = new Scanner(System.in);
         int numeroEntrada = 0;
-        int resultados = 0;
+        int resultado = 0;
         int control = 0;
 
-        System.out.print("Ingrese el número que desea calcular: ");
-        numeroEntrada = sc.nextInt();
+        System.out.print("Ingrese el numero que desea calcular: ");
+        numeroEntrada = scanner.nextInt();
 
         while (numeroEntrada >= control) {
-            resultados += control;
-            control++;
+            resultado = resultado + control;
+            control = control + 1;
         }
 
-        System.out.println("La suma de los numeros primeros " + numeroEntrada + " Sus números naturales es igual a " + resultados);
-
-        sc.close();
+        System.out.println("La suma de los primeros " + numeroEntrada + " naturales es igual a " + resultado);
+        
+        scanner.close();
     }
 }
