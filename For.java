9023af7b288280538e4a0f9342f701066e4d20a5
RIASLAB <117793990+RIASLAB@users.noreply.github.com>
2024-10-16 21:13:53 -0500

    For.java

--- For.java
+++ For.java
@@ -4,7 +4,7 @@ public class For {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int cantidadPersonas = 4;
+        int cantidadPersonas = 2;
         int edad = 0, estadoCivil = 0, estatura = 0, sexo = 0;
         double promedioEdad = 0, promedioEstatura = 0, porcentajePersonas = 0;
         int contadorPersonas = 0, acumuladorEdad = 0, acumuladorEstatura = 0;
@@ -21,21 +21,26 @@ public class For {
 
             System.out.print("Ingrese el sexo de la persona " + numeroPersona + " (1. Hombre, 2. Mujer): ");
             sexo = sc.nextInt();
-
-
+            
+                if (edad > 18 && estadoCivil == 1 && estatura > 170 && sexo == 1) {
+                contadorPersonas++;
+                acumuladorEdad += edad;
+                acumuladorEstatura += estatura;
+                }
         }
-
 
         if (contadorPersonas > 0) {
-            promedioEdad = (double) acumuladorEdad / contadorPersonas;
-            promedioEstatura = (double) acumuladorEstatura / contadorPersonas;
-        }
-
-        porcentajePersonas = ((double) contadorPersonas / cantidadPersonas) * 100;
+            promedioEdad = acumuladorEdad / contadorPersonas;
+            promedioEstatura = acumuladorEstatura / contadorPersonas;
+            porcentajePersonas = (contadorPersonas / (double)cantidadPersonas) * 100;
 
         System.out.println("El promedio de edad de las personas que cumplen es: " + promedioEdad + " años");
         System.out.println("El promedio de estatura de las personas que cumplen es: " + promedioEstatura + " cm");
         System.out.println("El porcentaje de las personas que cumplen es del: " + porcentajePersonas + " %");
+        }
+        else {
+             System.out.println("no hay personas que cumplan con la condicion.")
+                                }
 
         sc.close();
     }
